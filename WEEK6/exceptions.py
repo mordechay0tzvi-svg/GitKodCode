4aa3a243fcbefd3c3f89d0e368e97eb52e50e652
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-05-17 11:48:15 +0300

    last

--- WEEK6/exceptions.py
+++ WEEK6/exceptions.py
@@ -42,10 +42,16 @@ def parse_ints(values):
 
 #6
 def set_age(age):
-    if  150 <= age <= 0:
+    try:
+        age = int(age)
+    except ValueError:
+        raise ValueError("Age must be a number")
+    if  150 >= age >= 0:
         return age
     else:
-        raise ValueError
+        raise ValueError("Age must be between 0 and 150")
+
+print(set_age("shlomo"))
 
 #7
 class InsufficientFundsError(Exception):
@@ -57,6 +63,46 @@ def withdraw(balance, amount):
         return balance - amount
 
 #8
+def retry(func, n):
+    err = None
+    for _ in range(n):
+        try:
+            return func()
+        except Exception as e:
+            err = e
+    raise err
+
+
+
+#9
+def count_errors(funcs):
+    num_of_errors = 0
+    for func in funcs:
+        try:
+            func()
+        except:
+            num_of_errors += 1
+    return num_of_errors
+
+#print(count_errors( [lambda: 1, lambda: 1/0, lambda: int("x"), lambda: 2]))
+
+#10
+def load_config(path):
+    try:
+        with open(path, 'r') as f:
+            file = f.readlines()[0]
+            try:
+                file = int(file)
+            except ValueError:
+                file = None
+    except RuntimeError:
+        RuntimeError("failed to load config")
+
+
+
+
+
+
 
 
 
