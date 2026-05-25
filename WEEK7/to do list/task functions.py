531bdfffbbf7291e35d6977a9ae53d1a15301d3f
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-05-25 09:51:06 +0300

    add tast + updating load tasks to return list of dict

--- WEEK7/to do list/task functions.py
+++ WEEK7/to do list/task functions.py
@@ -1,7 +1,12 @@
 def load_tasks(filename):
     try:
         with open(filename, 'r') as f:
-            return f.readlines()
+            file = f.readlines()
+            tasks = []
+            for f in file:
+                task = {"id":f.split("|")[0], "status":f.split("|")[1], "description":f.split("|")[2]}
+                tasks.append(task)
+            return tasks
     except FileNotFoundError:
         print("File not found")
         return None
@@ -9,3 +14,10 @@ def load_tasks(filename):
 def save_tasks(filename, tasks):
     with open(filename, 'w') as f:
         f.write(tasks)
+
+def add_task(filename, description):
+    tasks = load_tasks(filename)
+    last_id = tasks[-1]["id"]
+    tasks.append({"id": last_id+1, "status":"pending", "description": description })
+    save_tasks(filename, tasks)
+
