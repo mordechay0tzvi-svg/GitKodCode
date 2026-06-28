dd955de701795f4823f77d5dff96332fd9a3a783
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-06-28 11:30:48 +0300

    2

--- C#/oop/classes/Program.cs
+++ C#/oop/classes/Program.cs
@@ -1,5 +1,5 @@
-﻿Track full = new Track(17, 412.5, 270);
-Track quick = new Track(8);
+﻿using System.Net.Quic;
+
 class Track
     {
     public int Id;
@@ -12,5 +12,21 @@ class Track
         Heading = heading;
         Console.WriteLine("Track created.");
     }
-    public Track(int id) : this(id, 0.0, 0.0) { }  
+    public Track(int id) : this(id, 0.0, 0.0) { }
+
+    public string ToString()
+    =>$"Track {Id}, Heading {Heading}, at {Speed}.";
+    
     }
+
+
+class program
+{
+    static void Main()
+    {
+        Track full = new Track(17, 412.5, 270);
+        Track quick = new Track(8);
+        Console.WriteLine(full.ToString());
+        Console.WriteLine(quick.ToString());
+    }
+}
