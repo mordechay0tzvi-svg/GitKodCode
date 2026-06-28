567ada0b3bf18fb54a517675576c0a4bd01f6829
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-06-28 11:11:57 +0300

    sunday morning

---
+++ C#/oop/classes/Program.cs
@@ -0,0 +1,16 @@
+﻿Track full = new Track(17, 412.5, 270);
+Track quick = new Track(8);
+class Track
+    {
+    public int Id;
+    public double Speed;
+    public double Heading;
+    public Track(int id, double speed, double heading)
+    {
+        Id = id;
+        Speed = speed;
+        Heading = heading;
+        Console.WriteLine("Track created.");
+    }
+    public Track(int id) : this(id, 0.0, 0.0) { }  
+    }
